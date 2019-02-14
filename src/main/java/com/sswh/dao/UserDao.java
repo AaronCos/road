09c5339f114dc668a50f6b7179aca18f20d4b776
AaronCos <821658917@qq.com>
2019-02-14 15:44:57 +0800

    添加注释

--- src/main/java/com/sswh/dao/UserDao.java
+++ src/main/java/com/sswh/dao/UserDao.java
@@ -5,7 +5,30 @@ import com.sswh.entity.User;
 import java.util.List;
 
 public interface UserDao {
+	/**
+	 * 插入一条完整的用户信息
+	 * @param user
+	 * @return
+	 */
 	public boolean regist(User user);
+
+	/**
+	 * 通过用户id获取一条用户的信息
+	 * @param iid
+	 * @return
+	 */
 	public User getByIid(int iid);
+
+	/**
+	 * 获取所有用户
+	 * @return
+	 */
 	public List<User> findAllUser();
+
+	/**
+	 * 批量获取用户的信息
+	 * @param iids
+	 * @return
+	 */
+	List<User> findByIids(List<Integer> iids);
 }
