91ff82fecde9807e76f8128656de52a99198741a
AaronCos <821658917@qq.com>
2019-02-14 14:44:10 +0800

    判断字符串为空和不为空的方法，增加字符串转数字列表方法

---
+++ src/main/java/com/sswh/utils/StringUtil.java
@@ -0,0 +1,48 @@
+package com.sswh.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 各类字符串的处理
+ * Created by wangchengcheng on 2019/2/14
+ */
+public class StringUtil {
+    /**
+     * 数字字符串转化为整型list
+     * @param numString
+     * @return List<Integer>
+     */
+    public static List<Integer> strToNumList(String numString){
+        if (numString == null){
+            return null;
+        }
+        ArrayList<Integer> iids = new ArrayList<>();
+        String[] split = numString.split(",");
+        for (int i = 0; i < split.length; i++) {
+            iids.add(Integer.valueOf(split[i]));
+        }
+        return iids;
+    }
+    public static boolean isNotEmpty(String param){
+        boolean isNotEmpty = false;
+        if(param != null){
+            String result = param.trim();
+            if(result != ""){
+                isNotEmpty = true;
+            }
+        }
+        return isNotEmpty;
+    }
+    public static boolean isEmpty(String param){
+        boolean isEmpty = true;
+        if(param != null){
+            String result = param.trim();
+            if(result != ""){
+                isEmpty = false;
+            }
+        }
+        return isEmpty;
+    }
+
+}
