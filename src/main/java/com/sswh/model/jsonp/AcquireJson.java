e9967c35dcd7e8c5c9b5eac36710e173e4c925a7
AaronCos <821658917@qq.com>
2019-07-14 23:30:36 +0800

    增加jsonp,解决跨域问题

--- src/main/java/com/sswh/model/jsonp/AcquireJson.java
+++ src/main/java/com/sswh/model/jsonp/AcquireJson.java
@@ -1,7 +1,36 @@
 package com.sswh.model.jsonp;
 
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by wangchengcheng on 2019/7/12
  */
+@Controller
+@RequestMapping("hellojson")
 public class AcquireJson {
+
+    @RequestMapping("json")
+    @ResponseBody
+    public List<Map> AcquireJson(){
+        Map<String, Object> map = new HashMap<>();
+        Map<String, Object> map1 = new HashMap<>();
+        List<Map> list = new ArrayList<>();
+        map.put("name","zhangsan");
+        map.put("age",12);
+        list.add(map);
+        map1.put("name","lisi");
+        map1.put("age",13);
+        list.add(map1);
+        String s = JSONObject.toJSON(list).toString();
+        //s = "success_jsonpCallback("+s+")";
+        return list;
+    }
 }
--- src/main/java/com/sswh/model/jsonp/JsonpAdvice.java
+++ src/main/java/com/sswh/model/jsonp/JsonpAdvice.java
@@ -1,7 +1,13 @@
 package com.sswh.model.jsonp;
 
+import org.springframework.web.servlet.mvc.method.annotation.AbstractJsonpResponseBodyAdvice;
+
 /**
  * Created by wangchengcheng on 2019/7/12
  */
-public class JsonpAdvice {
+//@ControllerAdvice(basePackageClasses = {Hello.class})
+public class JsonpAdvice extends AbstractJsonpResponseBodyAdvice {
+   public JsonpAdvice() {
+        super("callbackparam");
+    }
 }
