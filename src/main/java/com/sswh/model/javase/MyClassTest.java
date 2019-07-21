3d11bf5076d03493becbdc30934fdcdbb43340ba
AaronCos <821658917@qq.com>
2019-07-21 13:25:24 +0800

    java集合的使用

---
+++ src/main/java/com/sswh/model/javase/MyClassTest.java
@@ -0,0 +1,18 @@
+package com.sswh.model.javase;
+
+import java.util.*;
+
+/**
+ * Created by wangchengcheng on 2019/7/20
+ */
+public class MyClassTest  {
+
+    public void main(){
+        Map<String, Object> map = new HashMap<>();
+        Map<String, Object> linkedHashMap = new LinkedHashMap<>();
+        HashSet<String> strings = new HashSet<>();
+        ArrayList<Object> objects = new ArrayList<>();
+    }
+
+
+}
