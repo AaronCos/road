79e82366728d9ece0995bdda1ef34a8812bcea85
EnjoytheCode13123 <870639674@qq.com>
2019-12-24 11:25:57 +0800

    图片上传，成绩增加年级，无成绩新增不可编辑功能

---
+++ src/main/java/com/sswh/entity/GradeConfig.java
@@ -0,0 +1,26 @@
+package com.sswh.entity;
+
+import java.util.Date;
+
+
+public class GradeConfig {
+
+    private String grade;
+    private String subject;
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+}
