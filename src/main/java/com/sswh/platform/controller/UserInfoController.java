448fe7b84a42158e680fc07ce7dca0b36a64d212
AaronCos <821658917@qq.com>
2020-02-17 18:21:43 +0800

    整合路径

--- src/main/java/com/sswh/platform/controller/UserInfoController.java
+++ src/main/java/com/sswh/platform/controller/UserInfoController.java
@@ -1,4 +1,4 @@
-package com.sswh.platform.usercenter;
+package com.sswh.platform.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
