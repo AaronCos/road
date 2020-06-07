2a814730007dc34c8217016730b6493c459b409c
AaronCos <821658917@qq.com>
2020-06-07 12:35:55 +0800

    beetl

--- src/main/java/com/sswh/entity/PlatformGroup.java
+++ src/main/java/com/sswh/entity/PlatformGroup.java
@@ -1,9 +1,91 @@
 package com.sswh.entity;
 
 /**
- * Created by wangchengcheng on 2019/3/10
+ * Created by nuanfeng on 2019/3/10
  */
 public class PlatformGroup {
+
+
+    /**
+     * 主键
+     */
     private Integer iid;
-    private String GroupName;
+
+    /**
+     * 机构名称
+     */
+    private String name;
+
+    /**
+     * 机构描述
+     */
+    private String spec;
+
+    /**
+     * 父id
+     */
+    private Integer pid;
+
+    /**
+     * 父机构名称
+     */
+    private String pname;
+
+    /**
+     * 机构编码
+     */
+    private String codeId;
+
+    /**
+     * 排序号
+     */
+    private Integer orderid = 0;
+
+    /**
+     * 拼音的首字母
+     */
+    private String pinYin;
+
+    /**
+     * 附加属性 不计入数据库
+     */
+
+    /**
+     * 父机构编码
+     */
+    private String parCodeid = "";
+
+    /**
+     * 是否有下属机构
+     */
+    private Boolean isParent = false;
+    /**
+     * 机构级别
+     * @return
+     */
+    private Integer level = 0;
+
+    /**
+     * 部门负责人id
+     * @return
+     */
+    private Integer userid = 0;
+
+    /**
+     * 部门负责人姓名
+     * @return
+     */
+    private String userName = "";
+
+    /**
+     * 是否显示在通讯录
+     * @return
+     */
+    private Integer contactshow = 1;
+
+    /**
+     * 是否可删除
+     * @return
+     */
+    private Integer canDelete;
 }
